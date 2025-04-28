28a3a3b4a6f7a9b5e44c8e2bb76a784a16dc512e
Heitcleiff <nagisa.1881@gmail.com>
2025-04-28 15:27:37 +0700

    feature/package : updated package.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '**',
-        },
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
     ],
+  },
+  webpack: (config, { dev }) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    return config
   },
 };
 
